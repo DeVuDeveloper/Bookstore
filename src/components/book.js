7310e57034923d92a21c7df1d18797c180415c25
VuDej <drvu47@gmail.com>
2022-02-21 17:54:33 +0100

    added code to components

--- src/components/book.js
+++ src/components/book.js
@@ -1,4 +1,29 @@
 import '../style/book.css';
+// React component
+...
+// import useDispatch hook
+import { useDispatch } from 'react-redux';
+// import your Action Creators
+import {addBook, removeBook} from './redux/books/books';
+
+const dispatch = useDispatch();
+
+...
+
+const submitBookToStore = () => {
+    const newBook = {
+        id, // make sure it's unique
+        title,
+        author
+    }
+
+    // dispatch an action and pass it the newBook object (your action's payload)
+    dispatch(addBook(newBook));
+}
+
+...
+
+<button onClick={submitBookToStore}>Add Book</button>
 
 const Books = () => {
   const booksArr = [
