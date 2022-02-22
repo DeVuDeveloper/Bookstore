32042461eacf07d8d6d31730303158ad27e3f44e
VuDej <drvu47@gmail.com>
2022-02-22 16:36:00 +0100

    added const, action, reducer case for GET book from API

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,6 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const GET_BOOKS = 'bookStore/books/GET_BOOKS';
 
 export const initialState = [];
 
@@ -13,8 +14,15 @@ export const removeBook = (payload) => ({
   payload,
 });
 
+export const getBooks = (payload) => ({
+  type: GET_BOOKS,
+  payload,
+});
+
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_BOOKS:
+      return action.payload;
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
