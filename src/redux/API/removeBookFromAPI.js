dceca0042342bb3a63d29c189c0d6febbad74bde
VuDej <drvu47@gmail.com>
2022-02-24 21:22:58 +0100

    fix

--- src/redux/API/removeBookFromAPI.js
+++ src/redux/API/removeBookFromAPI.js
@@ -1,11 +1,12 @@
+/* eslint-disable camelcase */
 import { removeBook } from '../books/books';
 
-export const removeBookFromAPI = (id) => async (dispatch) => (
-  fetch(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/5zTkliAlHVC9mJgk6aLj/books/${id}`, {
+export const removeBookFromAPI = (item_id) => async (dispatch) => (
+  fetch(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/5zTkliAlHVC9mJgk6aLj/books/${item_id}`, {
     method: 'DELETE',
   })
     .then((response) => response.text())
-    .then(dispatch(removeBook(id)))
+    .then(dispatch(removeBook(item_id)))
 
 );
 export default removeBookFromAPI;
